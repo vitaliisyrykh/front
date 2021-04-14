7d95fdf0aec546f37de58fc9943a18f1bed201cc
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-14 22:37:59 +0300

    Add taskPage

---
+++ src/pages/taskPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskForm from '../components/TaskForm';
+
+const Task = props =>{
+  return(
+    <div>
+      <TaskForm/>
+    </div>
+  );
+}
+
+export default Task;

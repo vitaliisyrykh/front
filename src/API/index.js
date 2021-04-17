ed10a09001e43bda38a1ec061db6e2b6d2995134
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-17 15:30:29 +0300

    Add literalPath

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {BASE_URL} from '../config';
 
 const httpClient = axios.create({
-  baseURL: `${BASE_URL}/api`,
+  baseURL: 'http://localhost:3000/api',
 });
 
 export const createTask = async value => httpClient.post('/tasks/', value);

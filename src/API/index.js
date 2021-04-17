851f16a5a01ac43152c01b4d5d8a2a7dc20296ad
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-17 14:39:31 +0300

    Add config

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import {BASE_URL} from '../config';
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${BASE_URL}/api`,
 });
 
 export const createTask = async value => httpClient.post('/tasks/', value);
